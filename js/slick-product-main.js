4e3ed3bce1f66488ba7bba9f77ee92a3fcba44a5
Dogs-Design-Lab <dogs.design.labo@gmail.com>
2022-08-07 08:54:41 +0900

    商品ページのスライダーまで

---
+++ js/slick-product-main.js
@@ -0,0 +1,15 @@
+$jsProductMainSlider = $(".js-product-main");
+$jsProductMainSlider.slick({
+  fade: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 5000,
+});
+
+// サムネをクリックしたら、同じ番号の大画像を出す
+document.querySelectorAll('.js-product-thumb img').forEach((thumb,i) => {
+  thumb.addEventListener("click", () => {
+    $jsProductMainSlider.slick("slickGoTo", i, true);
+  })
+})
+
