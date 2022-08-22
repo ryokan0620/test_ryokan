c61d158c2a582740b92fef360a09d4ec7287735a
Dogs-Design-Lab <dogs.design.labo@gmail.com>
2022-08-22 22:56:59 +0900

    コメントアウト消しただけ

--- js/slick-top-main-carousel.js
+++ js/slick-top-main-carousel.js
@@ -2,10 +2,4 @@ $(".js-slick-main-carousel").slick({
   fade: true,
   arrows: false,
   asNavFor: ".js-slick-thumb-carousel",
-  // responsive: [
-  //   {
-  //     breakpoint: 768,
-  //     settings: "unslick",
-  //   },
-  // ],
 });
